521585122b4c03535efab693942e32b46b8a9a62
Danish Ahmed Khan <khand3826@gmail.com>
2026-02-19 14:02:04 +0530

    Fixed bugs in useEdiorStore.

---
+++ src/store/useEditorStore.ts
@@ -0,0 +1,87 @@
+import { create } from 'zustand'
+import { SchemaBuilder } from 'foldui-builder'
+import { NodeSpec } from 'foldui'
+
+type NodeSpecType = typeof NodeSpec
+
+type AddNodeOptions = {
+    type: string
+    props?: Record<string, unknown>
+    style?: Record<string, unknown>
+    responsive?: Record<string, unknown>
+}
+
+type EditorState = {
+    builder: SchemaBuilder<NodeSpecType>
+    version: number
+    selectedNodeId: string | null
+
+    selectNode: (id: string | null) => void
+
+    addNode: (options: AddNodeOptions, parentId: string) => string
+    removeNode: (id: string) => void
+
+    updateField: (id: string, field: string, value: unknown) => void
+    patchField: (id: string, field: string, patch: Record<string, unknown>) => void
+    patchPath: (id: string, path: string | (string | number)[], value: unknown) => void
+
+    getRenderSchema: () => unknown
+}
+
+export const useEditorStore = create<EditorState>((set) => {
+    const builder = new SchemaBuilder<NodeSpecType>(NodeSpec)
+
+    return {
+        builder,
+        version: 0,
+        selectedNodeId: null,
+
+        selectNode: (id) => set({ selectedNodeId: id }),
+
+        addNode: (options, parentId) => {
+            const id = builder.add(options).into(parentId)
+
+            set((state) => ({
+                version: state.version + 1,
+            }))
+
+            return id
+        },
+
+        removeNode: (id) => {
+            builder.remove(id)
+
+            set((state) => ({
+                version: state.version + 1,
+            }))
+        },
+
+        updateField: (id, field, value) => {
+            builder.updateField(id, field, value)
+
+            set((state) => ({
+                version: state.version + 1,
+            }))
+        },
+
+        patchField: (id, field, patch) => {
+            builder.patchField(id, field, patch)
+
+            set((state) => ({
+                version: state.version + 1,
+            }))
+        },
+
+        patchPath: (id, path, value) => {
+            builder.patchPath(id, path, value)
+
+            set((state) => ({
+                version: state.version + 1,
+            }))
+        },
+
+        getRenderSchema: () => {
+            return builder.toRenderSchema()
+        },
+    }
+})
